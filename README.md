ded16e91a2af89b0fd199ada0b96bfb5decb3f3a
neosolaris <batutah@gmail.com>
2022-10-09 17:43:28 +0900

    update codes, bug fix

--- README.md
+++ README.md
@@ -37,8 +37,9 @@ Usage: lemo v0.1
 -a|add "strings"             -- add memo
 -d|delete id[s]              -- delete id[s]
 -e|edit id[s]                -- edit id[s]
--l|list [d|w|m|y|a]          -- list
+-l|list [d|w|m|y|a|(n)]          -- list
    d:today w:week m:month y:year a:all
+   0:all, 1:in a day, 7:in a week
 -s|search keyword[s]         -- keyword[s] search: and-search
 -v|view id[s]                -- view id[s]
 -h|help                      -- this help
@@ -59,11 +60,12 @@ $ lemo -a foo bar hello world
 
 * list memo
 ```console
-$ lemo -l         # default: in this week
+$ lemo -l         # default: in 3 days
 $ lemo -l d(ay)   # in today
 $ lemo -l w(eek)  # in this week
 $ lemo -l m(onth) # in this month
 $ lemo -l y(ear)  # in this year
+$ lemo -l 5       # in a five days
 ```
 
 * search memo
--- lib/main.lua
+++ lib/main.lua
@@ -1,10 +1,11 @@
 #!/usr/bin/env luajit
 -- Luajit console memo program
--- update version: 0.1 (22-10-07)
+-- update version: 0.1 (22-10-09)
 -- first version: 0.1beta (22-10-05)
 
 -- ## TODO
 -- * 메모파일 암호화: 아주 나중에... 
+-- * [x] 22.10.09: do_list() 일수 별 입력 추가: 1 3 4 5 0
 -- * [x] 22.10.07: do_list(): 추가 옵션 기능 구현: today, week...
 -- * [x] 22.10.07: do_search(): ignorecase key search enabled: and search also
 -- * [x] 22.10.05: EDITOR 변수에 따라 do_edit()에서 처리하도록 설정
@@ -31,6 +32,14 @@ local DBFILE = PREFIX_DATA..'/memo.db'
 local progname = m.basename(PREFIX)
 local version = '0.1'
 --print(MCONF, MCONF_DATA)
+
+-- LIMIT OF DAYS default
+local DAY_LIMIT = 3
+-- COLOR SET OF DAYS
+local COL_TODAY = 'lred'
+local COL_INDAY = 'lpurple'
+local COL_LIMIT = 'lyellow'
+local COL_OTHER = 'lgray'
 
 --## under functions
 -- 출력 마지막에 결과를 표시
@@ -93,9 +102,9 @@ end
 
 -- 메모파일의 정보와 첫줄 표시
 local function fileinfo(id,f)
-  file = assert(io.open(PREFIX_DATA..'/'..f, 'r'))
+  local file = assert(io.open(PREFIX_DATA..'/'..f, 'r'))
   --title = file:read('*l')
-  first_line = file:read()
+  local first_line = file:read()
   file:close()
   if not first_line then first_line = '' end
   --local str = string.format(' '..i..' '..f..' ('..time2date(f)..')')
@@ -104,8 +113,16 @@ local function fileinfo(id,f)
   print('  '..first_line)
 end
 
+local function getfline(f)
+  local file = assert(io.open(PREFIX_DATA..'/'..f, 'r'))
+  local fline = file:read()
+  file:close()
+  if not fline then fline = '' end
+  return fline
+end
+
 -- 숫자로된 이름의 파일만 테이블을 만들어 반환
-local function get_flist(dir)
+local function getflist(dir)
   local flist = {}
   for f in m.listfiles(dir) do
     -- .과 ..은 넘어감
@@ -158,7 +175,7 @@ local function do_delete(ids)
   -- delete ids loop
   for _, id in pairs(ids) do
     id = tonumber(id)
-    for i, f in pairs(get_flist(PREFIX_DATA)) do
+    for i, f in pairs(getflist(PREFIX_DATA)) do
       if i == id then
         fileinfo(i,f)
         io.write(m.cstr('-> Are you sure (y/N)? ','lred'))
@@ -189,7 +206,7 @@ local function do_edit(ids)
     -- check if input id is number
     if id then
       -- loop list
-      for i, f in pairs(get_flist(PREFIX_DATA)) do
+      for i, f in pairs(getflist(PREFIX_DATA)) do
         --print(id, i, f)
         -- check id is in the list and edit
         if i == id then
@@ -210,6 +227,7 @@ local function do_edit(ids)
 end
 
 -- file is in date time
+--[[
 local function is_intime(ftime, limit)
   --ftime = 1665111863
   if limit == 'day' then
@@ -226,28 +244,76 @@ local function is_intime(ftime, limit)
     if os.date('%y%m%W') == os.date('%y%m%W',ftime) then return true end
   end
 end
+--]]
+
+-- file이 days 날수 이내라면 true 리턴: fitime, days are numbers
+local function is_intime(ftime, days)
+  days = days or 0
+  local q,_ = math.modf( (os.time() - ftime) / 86400 )
+  if days == 0 or q < days then
+    return true
+  end
+end
+
+-- file이 today인지 판별
+local function is_today(ftime)
+  if os.date('%Y%m%d',os.time()) == os.date('%Y%m%d',ftime) then
+    return true
+  end
+end
 
 -- list memo from data
 local function do_list(args)
-  local tot = 0
+  local tot, tot_today, tot_1day, tot_limit = 0,0,0,0
   -- set limit: default w(eek)
-  local limit = args[1] or 'w'
-  if args[1] == 'd' or args[1] == 'day' then limit = 'day'
-  elseif args[1] == 'w' or args[1] == 'week' then limit = 'week'
-  elseif args[1] == 'm' or args[1] == 'month' then limit = 'month'
-  elseif args[1] == 'y' or args[1] == 'year' then limit = 'year'
-  elseif args[1] == 'a' or args[1] == 'all' then limit = 'all'
-  else limit = 'week' end
-
-  -- loop file list
-  for i,f in pairs(get_flist(PREFIX_DATA)) do
-    if is_intime(f,limit) then
-      fileinfo(i,f)
-      tot = tot + 1
+  --local limit = args[1] or '3'
+  local limit = tonumber(args[1])
+  -- if no args, limit is default = 3 days
+  if #args == 0 then limit = DAY_LIMIT end
+  --- check arg[1] is valid
+  if not limit then
+    if args[1] == 'd' or args[1] == 'day' then limit = 1
+    elseif args[1] == 'w' or args[1] == 'week' then limit = 7
+    elseif args[1] == 'm' or args[1] == 'month' then limit = 30
+    elseif args[1] == 'y' or args[1] == 'year' then limit = 365
+    elseif args[1] == 'a' or args[1] == 'all' then limit = 0
+    else print('-> option is not accepted! See help: -h'); return
     end
   end
 
-  print_title('list', '['..limit..'] total:'..tot)
+  -- loop file list
+  local fline = ''
+  local icon = ''
+  for i,f in pairs(getflist(PREFIX_DATA)) do
+    tot = tot + 1
+    -- check file is in limit days
+    --if is_intime(tonumber(f),limit) then
+    if is_intime(f,limit) then
+      tot_limit = tot_limit + 1
+      fline = getfline(f) or ''
+      --outline = string.format('%4d  %s %s', i, time2date(f), fline)
+      outline = string.format('%4d  %s', i, fline)
+      -- show color days in 1 day files
+      if is_intime(f, 1) then
+        if is_today(f) then
+          icon = m.cstr('', COL_TODAY)
+          tot_today = tot_today + 1
+        else
+          icon = m.cstr('', COL_INDAY)
+          tot_1day = tot_1day + 1
+        end
+      else
+        if is_intime(f, DAY_LIMIT) then
+          icon = m.cstr('', COL_LIMIT)
+        else
+          icon = m.cstr('', COL_OTHER)
+        end
+      end
+      print(string.format('%4d %s %s',i,icon,fline))
+    end
+  end
+
+  print_title('list', string.format('total=%d, limit=%d, yesterday=%d, today=%d', tot, tot_limit, tot_1day, tot_today))
   --print_title(string.format('list[%s]',limit
 end
 
@@ -292,14 +358,12 @@ end
 local function do_search(keys)
   --local lines = {}
   local tot = 0
-  for i,f in pairs(get_flist(PREFIX_DATA)) do
+  for i,f in pairs(getflist(PREFIX_DATA)) do
     -- 검색해서 해당라인들을 테이블로 넘겨줌
     local lines = match_lines(PREFIX_DATA..'/'..f, keys)
     if lines then
       tot = tot + 1
-      --local str = string.format(' '..i..' '..f..' ('..time2date(f)..')')
-      local str = string.format(' %d %s (%s)', i, f, time2date(f))
-      m.cprint(str, 'yellow')
+      m.cprint(string.format('%4d  %s %s', i, f, time2date(f)))
       for _, line in pairs(lines) do
         print('  '..line)
       end
@@ -317,14 +381,15 @@ end
 -- 메모 보기
 local function do_view(ids)
   local tot = 0
+  local icon = ''
   for _,id in pairs(ids) do
     id = tonumber(id)
-    for i,f in pairs(get_flist(PREFIX_DATA)) do
+    for i,f in pairs(getflist(PREFIX_DATA)) do
       if i == id then
         tot = tot + 1
-        m.cprint(i..') '..f)
+        m.cprint(string.format('%4d  %s %s', i, f, time2date(f)))
         for line in io.lines(PREFIX_DATA..'/'..f) do
-          print('  '..line) 
+          print('  '..line)
         end
       end
     end
